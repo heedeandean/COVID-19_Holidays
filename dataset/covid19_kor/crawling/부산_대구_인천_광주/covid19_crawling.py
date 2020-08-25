144a40e35516d6f432869734b866269a34589544
alpha-94 <alpha-94@users.noreply.github.com>
2020-08-26 02:26:27 +0900

    2020-08-26

--- dataset/covid19_kor/crawling/부산_대구_인천_광주/covid19_crawling.py
+++ dataset/covid19_kor/crawling/부산_대구_인천_광주/covid19_crawling.py
@@ -4,10 +4,13 @@ import ssl
 import numpy as np
 import re
 import sys
-import numpy
+import pandas as pd
+import datetime as dt
 
 # 콘솔 창에서 배열 다 확인 할 수 있도록 제공
-numpy.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(threshold=sys.maxsize)
+pd.set_option('display.max_row', 500)
+pd.set_option('display.max_columns', 100)
 
 
 
@@ -37,7 +40,7 @@ a_find_ic = soup_ic.find_all('a', class_ ='patinet-profile')
 ## 리스트 초기화
 list_data_bs = []
 list_data_ic = []
-list_data_gj = []
+# list_data_gj = []
 
 
 ## 부산시 parser
@@ -120,15 +123,103 @@ arr_data_bs = np.array(list_data_bs).reshape(-1,5)
 arr_data_ic = re_inchon(list_data_ic) # 전처리함수 내 배열형 자동 변환
 
 
-
-
-# Save as csv = 매번 날짜 변경 (덮어쓰기하면 예전꺼 날라가서 방법 몰라서 수작업으로 함 .. )
-np.savetxt('bs_data_20200824.csv', arr_data_bs, fmt='%s', delimiter=",", encoding='UTF-8') # 부산시 데이터
-np.savetxt('ic_data_20200824.csv', arr_data_ic, fmt='%s', delimiter=",", encoding='UTF-8') # 인천시 데이터
-
+# Save as csv = 매번 날짜 변경 (파일명 : 타입_데이터_지역_날짜)
+np.savetxt('busan_arr/arr_data_bs_20200825.csv', arr_data_bs, fmt='%s', delimiter=",", encoding='UTF-8') # 부산시 데이터
+np.savetxt('inchon_arr/arr_data_bs_20200825.csv', arr_data_ic, fmt='%s', delimiter=",", encoding='UTF-8') # 인천시 데이터
 
 
 
+# 데이터 프레임으로 변환
+col_name = ['area','cfmDate','route','contractCnt'] # 지역 / 확진일 / 감염경로 / 접촉자 수
+
+
+## 부산시
+bs_df = pd.DataFrame(arr_data_bs[1:,(0,4,1,2)] , columns=col_name) # 0 : 지역 - 확진번호 / 4: 확진일 / 1: 감염 경로 / 2: 접촉자 수
+
+## 인천시
+ic_df = pd.DataFrame(arr_data_ic[:,(1,2,3)],columns=col_name[:-1])
+ic_df.insert(3,col_name[3],'-') # 접촉자 수 없음
+ic_df['area'] = '인천' # ~구 -> 인천으로 변경
+
+
+# 데이터프레임 전처리
+
+## 부산시
+
+def bs_df_regex(bs_df):
+
+    for i in bs_df.index:
+        if bs_df.loc[i,'cfmDate'] == '':
+            bs_df.loc[i,'cfmDate'] = '-'
+
+    # 1. xx..-  :: 표현식이 아닌 경우 삭제
+    p = re.compile(r'[\D+]-')
+    for i in bs_df.index :
+        if p.search(bs_df.loc[i,'area']) == None:
+            bs_df = bs_df.drop(index=i)
+
+    # 2. -00..  :: 표현식 삭제
+    p = re.compile(r'-[\d+]')
+    for i in bs_df.index :
+        bs_df.loc[i,'area'] = p.sub('',bs_df.loc[i,'area'])
+
+    # 3. 숫자 표현식 삭제
+    p = re.compile(r'\d+')
+    for i in bs_df.index :
+        bs_df.loc[i, 'area'] = p.sub('',bs_df.loc[i, 'area'])
+
+    # 4. (xxx...) :: (강서구 등) 표현식 삭제
+    p = re.compile(r'[(]\D+[)]')
+    for i in bs_df.index :
+        bs_df.loc[i, 'area'] = p.sub('', bs_df.loc[i, 'area'])
+
+    # 5. 띄어쓰기 삭제
+    p = re.compile(r'\s+')
+    for i in bs_df.index :
+        bs_df.loc[i, 'area'] = p.sub('', bs_df.loc[i, 'area'])
+
+    # 6. 타지역 발생자 삭제  / 삭제 사유 : 타지역 발생자 데이터는 제공하지 않음
+    p = re.compile(r'부산')
+    for i in bs_df.index :
+        if p.search(bs_df.loc[i,'area']) == None:
+            bs_df = bs_df.drop(index=i)
+
+    # 7. 날짜 표현 변경
+    p1 = re.compile(r'\d+')
+    for i in bs_df.index:
+        month, day = p1.findall(bs_df.loc[i, 'cfmDate'])
+        year = str(dt.datetime.now().year)
+        hypn = '-'
+        cfmdate = year + hypn + month + hypn + day
+        bs_df.loc[i, 'cfmDate'] = cfmdate
+
+    return bs_df
+
+bs_df = bs_df_regex(bs_df)
+
+## 인천시
+def ic_df_regex(ic_df):
+    # 1. 날짜 표현 변경
+    p = re.compile(r'\d+')
+    for i in ic_df.index :
+        year, month, day = p.findall(ic_df.loc[i,'cfmDate'])
+        date = dt.datetime(int(year),int(month),int(day)).strftime('%Y-%m-%d')
+        ic_df.loc[i,'cfmDate'] = date
+
+    # 2. 감염경로 빈 값 '-' 로 채우기
+    for i in ic_df.index:
+        if ic_df.loc[i, 'route'] == '':
+            ic_df.loc[i, 'route'] = '-'
+
+    return ic_df
+
+ic_df = ic_df_regex(ic_df)
+
+
+
+
+bs_df.to_csv('busan_df/df_data_bs_20200825.csv',encoding='UTF-8')
+ic_df.to_csv('inchon_df/df_data_ic_20200825.csv',encoding='UTF-8')
 
 
 
